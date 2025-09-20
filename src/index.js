917b220d71ebd24c853d28b355e5452fe520c15c
SoyQuillo <santiagojose190305@gmail.com>
2025-09-20 14:57:52 -0500

    Arrays en react

--- src/index.js
+++ src/index.js
@@ -2,21 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
-import { Button } from './Button'
-import { TaskCard } from './Task'
+import { Button } from "./Button";
+import { TaskCard } from "./Task";
 import { Saludar } from "./Saludar";
 import { func } from "prop-types";
-import {Post} from './Post'
+import { Post } from "./Post";
 
 //<> {} []
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const users = [
+  {
+    id: 1,
+    name: "Santiag Jose",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Juan Jose",
+    image: "https://robohash.org/user2",
+  },
+];
 
 root.render(
-  <>  
-      <Post/>
+  <>
+    {users.map((user, index) => {
+      return (
+        <div key={index}>
+          <h1>{user.name} </h1>
+          <img src={user.image} />
+        </div>
+      );
+    })}
   </>
-
-
 );
