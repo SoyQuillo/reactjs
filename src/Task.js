fc308bdce9478c66bb3107dfdf7a107d6a429192
SoyQuillo <santiagojose190305@gmail.com>
2025-09-20 10:04:15 -0500

    primera forma de estilos en react

---
+++ src/Task.js
@@ -0,0 +1,14 @@
+export function TaskCard(){
+
+    const cardStyles = {
+        background: '#202020', color: '#fff', padding: '20px'
+    }
+
+    const tittleStyle = {fontWeight: "lighter"}
+
+
+    return <div style={cardStyles}>
+       <h1 style={}>Mi primer tarea</h1> 
+       <p>Tarea realizada</p>
+    </div>
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
 import { Button } from './Button'
+import { TaskCard } from './Task'
 
 //<> {} []
 
@@ -10,28 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-
-    <Button text="CLICK ME"/>
-    <Button text= "PAY"/>
-    <Button text = "HELLO WORLD" name="Santiago"/>
-
-{/*     <UserCard
-      name="Quillosky"
-      amount={20000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 Main Street", city: "New York" }}
-      greet={function () {
-        alert("Hello");
-      }}
-    />
-
-    <UserCard
-      name="Joe"
-      amount={15000}
-      married={false}
-      points={[100, 32.3, 21.2]}
-      address={{ street: "av some", city: "New York" }}
-    /> */}
+<TaskCard/>
   </>
 );
