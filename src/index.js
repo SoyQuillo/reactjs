5cf26a05ffbe60fad80c6b6de642da6cbc2d43e1
SoyQuillo <santiagojose190305@gmail.com>
2025-10-06 09:22:23 -0500

    useState primeros pasos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
@@ -12,28 +12,25 @@ import { Post } from "./Post";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const users = [
-  {
-    id: 1,
-    name: "Santiag Jose",
-    image: "https://robohash.org/user1",
-  },
-  {
-    id: 2,
-    name: "Juan Jose",
-    image: "https://robohash.org/user2",
-  },
-];
+function Counter() {
+
+  const [counter , setCounter] = useState(10);
+
+  
+  return(
+    <div>
+     <h1>Counter: {counter} </h1>
+    <button onClick={() =>{
+      setCounter(counter + 10)
+    } }>Sumar</button>
+    <button onClick={() => setCounter(counter - 10) }>Restar</button>
+    <button onClick={() => setCounter(1000) }>Reiniciar</button>
+    </div>
+  )
+}
 
 root.render(
   <>
-    {users.map((user, index) => {
-      return (
-        <div key={index}>
-          <h1>{user.name} </h1>
-          <img src={user.image} />
-        </div>
-      );
-    })}
+  <Counter/>
   </>
 );
