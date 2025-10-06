d773ae8bce60d4782b95300af2e0ced034cae579
SoyQuillo <santiagojose190305@gmail.com>
2025-10-06 09:55:18 -0500

    useEffect primeros pasos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
@@ -15,12 +15,25 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 function Counter() {
 
   const [mensaje, setMensaje] = useState('');
+  const [counter, setCounter ] = useState(0);
+
+  useEffect(() =>{
+    console.log('render')
+  }, [counter])
 
   
   return(
     <div>
     <input onChange={e => setMensaje(e.target.value) }/>
     <button onClick={() => alert('El mensaje es: ' + mensaje)}>Save</button>
+
+    <hr></hr>
+
+
+    <h1>Counter: {counter} </h1>
+    <button onClick={() =>{
+      setCounter(counter+1)
+    }}>Incrementar</button>
 
     </div>
   )
